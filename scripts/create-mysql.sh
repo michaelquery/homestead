0b51ed5394f11a41c361373930fc343d6513cca3
Michael Query <michaelquery@gmail.com>
2016-11-08 17:39:18 -0700

    Update create-mysql.sh

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -9,4 +9,6 @@ EOF
 
 DB=$1;
 
+mysql -uhomestead -psecret -e "SET PASSWORD  = PASSWORD('secret')" --connect-expired-password;
+mysql -uroot -psecret -e "SET PASSWORD  = PASSWORD('secret')" --connect-expired-password;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
